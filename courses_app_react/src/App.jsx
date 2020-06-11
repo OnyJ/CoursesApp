36125543afe80d1060dccf47625de32983570045
OnyJ <julien.josselin75@hotmail.fr>
2020-06-11 18:33:48 +0200

    create Home.jsx page

--- courses_app_react/src/App.jsx
+++ courses_app_react/src/App.jsx
@@ -1,39 +1,37 @@
-import React from 'react';
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Logout from './pages/Logout'
-import AdminDashboard from './pages/AdminDashboard'
-import Navbar from './components/Navbar'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link
-} from "react-router-dom";
+import React from "react";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import AdminDashboard from "./pages/AdminDashboard";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 const App = () => {
   return (
-      <Router>
-        <div>
-          <Navbar/>
-          <Switch>
-            <Route path='/register'>
-              <Register/>
-            </Route>
-            <Route path='/login'>
-              <Login/>
-            </Route>
-            <Route path='/logout'>
-              <Logout/>
-            </Route>
-            <Route path='/AdminDashboard'>
-              <AdminDashboard/>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-   
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/logout">
+            <Logout />
+          </Route>
+          <Route path="/AdminDashboard">
+            <AdminDashboard />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
-export default App
+export default App;
