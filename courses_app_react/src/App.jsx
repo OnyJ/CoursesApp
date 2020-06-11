95fd3a267d996aa26583d4eb6278d66079919525
Robin Dercole <robin.dercole@gmail.com>
2020-06-11 12:18:37 +0200

    router started

--- courses_app_react/src/App.jsx
+++ courses_app_react/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Logout from './pages/Logout'
 
 const App = () => {
   return (
-    <Login/>
+    <div>
+      <Register/>
+      <Login/>
+      <Logout/>
+    </div>
   );
 }
 
---
+++ courses_app_react/src/pages/Logout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logout = () => {
+
+    return(
+        <h1>Log out page</h1>
+    )
+
+}
+
+export default Logout;
